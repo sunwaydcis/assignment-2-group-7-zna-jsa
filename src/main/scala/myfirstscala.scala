4354289005e7ce4b9316a99dc588cfac38aba3c2
Zar Nie Aung <zarzukyi3@gmail.com>
2024-11-19 17:54:20 +0800

    Start
    Just the names for now

--- src/main/scala/myfirstscala.scala
+++ src/main/scala/myfirstscala.scala
@@ -1,10 +1,5 @@
-import scalafx.application.JFXApp3
-import scalafx.application.JFXApp3.PrimaryStage
-
-object MyApp extends JFXApp3:
-
-  override def start(): Unit =
-    stage = new PrimaryStage()
-
-
-end MyApp
+/*
+This Assignment is from:
+- 21074380 : Zar Nie Aung
+- ID Here : Joey Simon Abrahams
+ */
