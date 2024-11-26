a1a8d574038d92cf396beb3ab615fc9d9abf03bc
Zar Nie Aung <zarzukyi3@gmail.com>
2024-11-26 10:51:51 +0800

    First Full functioning
    Changes:
    Implemented data parser (from String to HospitalData) using each index of the headerRow.
    
    Rectified: converted Date to String instead of LocalDate. LocalDate parse was failing, so convert to String as no strict need to use it.
    
    Future movements:
    Output for average state is still HashMap. Needs to be converted into a readable form.

--- src/main/scala/myfirstscala.scala
+++ src/main/scala/myfirstscala.scala
@@ -1,5 +1,6 @@
 import java.io
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import scala.io.Source
 
 //Reading the file. (TESTING)
@@ -7,14 +8,15 @@ val hopsitalDataset = Source.fromFile("C:/Users/User/Downloads/hospital.csv").ge
 
 trait CSVFileReader[T]:
   /** Reads the CSVFile and converts it into a list of type T.
-   *  Why? I thought it was the best approach to take.
+   *  Why? I thought it was the best approach to take. It demonstrates Parametric Polymorphism
    */
   def processFile(source: String): List[T]
   
 
 //Data Model to be operated on
 //Why keep all data? - Under the assumption that the DataAnalysis operations MAY increase to other fields.
-case class HospitalData( val date: LocalDateTime,
+//Starting to not see a need for it
+case class HospitalData( val date: String,
                          val state: String,
                          val beds: Int,
                          val covidBeds: Int,
@@ -31,6 +33,41 @@ case class HospitalData( val date: LocalDateTime,
   //Get the method to get total beds, removes the need to constantly run (record => record.beds + ...)
   def getTotalBeds = beds+covidBeds+nonCritBeds
 
+//Processes File Hospital.csv ONLY.
+object HospitalCSVReader extends CSVFileReader[HospitalData]:
+  //Will process header accordingly.
+  override def processFile(source: String): List[HospitalData] =
+    val records = Source.fromFile(source).getLines().toList
+    val headerRow = records.head.split(',').map(_.trim)
+    val recordRows = records.tail
+
+    //Parses each line into a data field of the HospitalData
+    recordRows.map {
+      row => val fields = row.split(',').map(_.trim)
+        HospitalData(
+//          date = LocalDateTime.parse(fields(headerRow.indexOf("date")), DateTimeFormatter.ofPattern("MM-dd-yyyy")),
+          date = fields(headerRow.indexOf("date")),
+          state = fields(headerRow.indexOf("state")),
+          beds = fields(headerRow.indexOf("beds")).toInt,
+          covidBeds = fields(headerRow.indexOf("beds_covid")).toInt,
+          nonCritBeds = fields(headerRow.indexOf("beds_noncrit")).toInt,
+          puiAdmissions = fields(headerRow.indexOf("admitted_pui")).toInt,
+          covidAdmissions = fields(headerRow.indexOf("admitted_covid")).toInt,
+          totalAdmissions = fields(headerRow.indexOf("admitted_total")).toInt,
+          puiDischarged = fields(headerRow.indexOf("discharged_pui")).toInt,
+          covidDischarged = fields(headerRow.indexOf("discharged_covid")).toInt,
+          totalDischarged = fields(headerRow.indexOf("discharged_total")).toInt,
+          hospCovid = fields(headerRow.indexOf("hosp_covid")).toInt,
+          hospPui = fields(headerRow.indexOf("hosp_pui")).toInt,
+          hospNonCovid = fields(headerRow.indexOf("hosp_noncovid")).toInt
+        )
+    }
+
+
+
+
+    //Get the header and prepare data parsing
+
 
 //Responsible for all DataAnalysis Operations for the Hospital
 object HospitalDataAnalysis:
@@ -43,14 +80,23 @@ object HospitalDataAnalysis:
     data.maxBy(record => record.getTotalBeds).state
 
   def getCovidBedRatio(data: List[HospitalData]): Double =
-    val (totalBeds, totalCovidBeds) = data.foldLeft((0, 0)) { (acc, record) =>
-      (acc._1 + record.getTotalBeds, acc._2 + record.covidBeds)
+    /** Use of FoldLeft
+     * Why? Runs through list once, updates the accumulator tuple.
+     */
+    val (totalBeds, totalCovidBeds) = data.foldLeft((0, 0)) { (cumulative, record) =>
+      (cumulative._1 + record.getTotalBeds, cumulative._2 + record.covidBeds)
     }
 
+    //Divide-By-Zero error prevention
     if (totalBeds == 0) 0.0
     else totalCovidBeds.toDouble / totalBeds
 
   def getAverageAdmissionsByCategory(data: List[HospitalData]): Map[String, List[Double]] =
+    /** How?
+     * Group by all records by their state name
+     * So far, similar to old CovidBedRatio, it uses .map (making it run through the list 4 times)
+     * It is easier to manage, but poorer performance (if the record get's any larger)
+     */
     data.groupBy(_.state).map { (state, records) =>
       state -> List(
         records.map(_.covidAdmissions).sum.toDouble / records.size,
@@ -61,12 +107,14 @@ object HospitalDataAnalysis:
 
 object Runner extends App:
   //Test data
-  val dataset = List(
-    HospitalData(LocalDateTime.now(), "Johor", 500, 300, 350, 30, 20, 50, 15, 10, 25, 5, 10, 15),
-    HospitalData(LocalDateTime.now(), "Johor", 500, 150, 350, 40, 25, 65, 20, 15, 35, 10, 15, 20),
-    HospitalData(LocalDateTime.now(), "Kedah", 400, 100, 300, 20, 15, 35, 10, 5, 15, 3, 7, 10),
-    HospitalData(LocalDateTime.now(), "Kedah", 400, 100, 300, 25, 20, 45, 12, 8, 20, 4, 9, 12)
-  )
+//  val dataset = List(
+//    HospitalData(LocalDateTime.now(), "Johor", 500, 300, 350, 30, 20, 50, 15, 10, 25, 5, 10, 15),
+//    HospitalData(LocalDateTime.now(), "Johor", 500, 150, 350, 40, 25, 65, 20, 15, 35, 10, 15, 20),
+//    HospitalData(LocalDateTime.now(), "Kedah", 400, 100, 300, 20, 15, 35, 10, 5, 15, 3, 7, 10),
+//    HospitalData(LocalDateTime.now(), "Kedah", 400, 100, 300, 25, 20, 45, 12, 8, 20, 4, 9, 12)
+//  )
+
+  val dataset = HospitalCSVReader.processFile("C:/Users/User/Downloads/hospital.csv")
 
   val higheststate = HospitalDataAnalysis.getStateWithHighestBedCount(dataset)
   val ratio = HospitalDataAnalysis.getCovidBedRatio(dataset)
