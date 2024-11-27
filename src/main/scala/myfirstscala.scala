32a03a1021f90b333cc55b0c6db55b0f9693c890
Zar Nie Aung <zarzukyi3@gmail.com>
2024-11-27 15:46:41 +0800

    Naming Fixes
    Removed uneccesary totalBeds. Removed BigDecimal making the code 3 seconds faster. Used %.2f for faster rounding (inittially used s"" and not f"")
    
    Going to make the following:
    Use direct map based transformation for each record collected.
    Turn average method to use fold left to run through the list once.
    
    If needed, use LazyList or ListBuffer as rumoured to be faster for this kind off job

--- src/main/scala/myfirstscala.scala
+++ src/main/scala/myfirstscala.scala
@@ -1,16 +1,14 @@
 import scala.io.Source
-////Reading the file. (TESTING)
-//val hospitalDataset = Source.fromFile("C:/Users/User/Downloads/hospital.csv").getLines().toList
 
 trait CSVFileReader[T]:
   /** Reads the CSVFile and converts it into a list of type T.
    *  Why? I thought it was the best approach to take. It demonstrates Parametric Polymorphism
    */
   def processFile(source: String): List[T]
-
-//Data Model to be operated on
-//Why keep all data? - Under the assumption that the DataAnalysis operations MAY increase to other fields.
-//Starting to not see a need for it
+/** Data Model to be operated on
+* Why keep all data? - Under the assumption that the DataAnalysis operations MAY increase to other fields.
+* Starting to not see a need for it
+ */
 case class HospitalData( date: String,
                          state: String,
                          beds: Int,
@@ -24,23 +22,23 @@ case class HospitalData( date: String,
                          totalDischarged: Int,
                          hospPui: Int,
                          hospCovid: Int,
-                         hospNonCovid: Int):
-  //Get the method to get total beds, removes the need to constantly run (record => record.beds + ...)
-  def getTotalBeds: Int = beds + covidBeds + nonCritBeds
+                         hospNonCovid: Int)
 
 //Processes File Hospital.csv ONLY.
 object HospitalCSVReader extends CSVFileReader[HospitalData]:
   //Will process header accordingly.
   override def processFile(source: String): List[HospitalData] =
     val records = Source.fromFile(source).getLines().toList
-    val headerRow = records.head.split(',').map(_.trim)  //Get the header and prepare data parsing
+    val headerRow = records.head.split(',').map(_.trim)  //Get the header and prepare data parsing //Need buffered lists
     val recordRows = records.tail
 
     //Parses each line into a data field of the HospitalData
     recordRows.map {
       row => val fields = row.split(',').map(_.trim)
         HospitalData(
-          //Drawbacks: THE COLUMN MUST EXIST. Cons: Not Robust.
+          /** Drawbacks: THE COLUMN MUST EXIST. Cons: Not Robust.
+           * Use getOrElse()
+           */
           // date = LocalDateTime.parse(fields(headerRow.indexOf("date")), DateTimeFormatter.ofPattern("MM-dd-yyyy")),
           date = fields(headerRow.indexOf("date")),
           state = fields(headerRow.indexOf("state")),
@@ -61,73 +59,74 @@ object HospitalCSVReader extends CSVFileReader[HospitalData]:
 
 //Responsible for all DataAnalysis Operations for the Hospital
 object HospitalDataAnalysis:
-  def getStateWithHighestBedCount(data: List[HospitalData]) : String =
-    /** Calculates total bed count for each record and retrieves the name of the state with the highest total.
-     * How:
-     * Runs through the list once
-     * Returns whichever record has the maximum sum of beds (beds + covid beds + non-critical beds)
-     */
-    data.maxBy(record => record.getTotalBeds).state
-    //No try/catch/fail-safe needed
-
-  def getCovidBedRatio(data: List[HospitalData]): Double =
-    /** Use of FoldLeft
-     * Why? Runs through list once, updates the accumulator tuple.
-     */
+  def stateWithHighestBedCount(data: List[HospitalData]) : String =
+    data.maxBy(_.beds).state
+  /** Calculates total bed count for each record and retrieves the name of the state with the highest total.
+   * How:
+   * Runs through the list once
+   * Returns whichever record has the maximum sum of beds (beds + covid beds + non-critical beds)
+   * No try/catch/fail-safe needed
+   */
+  def covidBedRatio(data: List[HospitalData]): Double =
     val (totalBeds, totalCovidBeds) = data.foldLeft((0, 0)) { (cumulative, record) =>
-      (cumulative._1 + record.getTotalBeds, cumulative._2 + record.covidBeds)
+      (cumulative._1 + record.beds, cumulative._2 + record.covidBeds)
     }
     if (totalBeds == 0) 0.0 //Divide-By-Zero error prevention
     else totalCovidBeds.toDouble / totalBeds
-
-  def getAverageAdmissionsByCategory(data: List[HospitalData]): Map[String, List[Double]] =
-    /** How?
-     * Group by all records by their state name
-     * So far, similar to old CovidBedRatio, it uses .map (making it run through the list 4 times)
-     * It is easier to manage, but poorer performance (if the record gets any larger)
-     */
+  /** Use of FoldLeft
+   * Why? Runs through list once, updates the accumulator tuple.
+   */
+  def averageAdmissionsByCategory(data: List[HospitalData]): Map[String, List[Double]] =
     data.groupBy(_.state).map { (state, records) =>
+      val totalSize = records.size
       state -> List(
         //Rounds to 2 decimal places
-        BigDecimal(records.map(_.covidAdmissions).sum.toDouble / records.size).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble,
-        BigDecimal(records.map(_.puiAdmissions).sum.toDouble / records.size).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble,
-        BigDecimal(records.map(_.totalAdmissions).sum.toDouble / records.size).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+        records.map(_.covidAdmissions).sum.toDouble / totalSize,
+        records.map(_.puiAdmissions).sum.toDouble / totalSize
       )
     }
 
-object Runner extends App:
-  //Test data
-//  val dataset = List(
-//    HospitalData(LocalDateTime.now(), "Johor", 500, 300, 350, 30, 20, 50, 15, 10, 25, 5, 10, 15),
-//    HospitalData(LocalDateTime.now(), "Johor", 500, 150, 350, 40, 25, 65, 20, 15, 35, 10, 15, 20),
-//    HospitalData(LocalDateTime.now(), "Kedah", 400, 100, 300, 20, 15, 35, 10, 5, 15, 3, 7, 10),
-//    HospitalData(LocalDateTime.now(), "Kedah", 400, 100, 300, 25, 20, 45, 12, 8, 20, 4, 9, 12)
-//  )
+//  def averageAdmissionsByCategory2(data: List[HospitalData]): Map[String, List[Double]] =
+//    data.groupBy(_.state).foldLeft(0.0, 0.0, 0.0){
+//
+//    }
+/** How?
+ * Group by all records by their state name
+ * So far, similar to old CovidBedRatio, it uses .map (making it run through the list 4 times)
+ * It is easier to manage, but poorer performance (if the record gets any larger)
+ */
 
+//object Runner extends App:
+@main def main(): Unit =
   //To remove WARNING - made private (as it is only owned by the runner object - not needed but nice)
-  private val dataset = HospitalCSVReader.processFile("C:/Users/User/Downloads/hospital.csv")
-
-  //This approach can still be used - Better readability, uses memory
-  val highestState = HospitalDataAnalysis.getStateWithHighestBedCount(dataset)
-  val ratio = HospitalDataAnalysis.getCovidBedRatio(dataset)
-  val averages = HospitalDataAnalysis.getAverageAdmissionsByCategory(dataset)
-
-  //test printing
-//  println(s"$highestState, \n$ratio \n$averages")
+  val dataset = HospitalCSVReader.processFile("C:/Users/User/Downloads/hospital.csv")
 
 //Why this approach? Wanted to try going for Functional Programming paradigms to improve efficiency and memory usage.
-  println(s"State with the highest bed count: ${HospitalDataAnalysis.getStateWithHighestBedCount(dataset)}\n" +
-    s"Average Covid to Bed Ratio overall: ${HospitalDataAnalysis.getCovidBedRatio(dataset)}\n" +
+  println(f"State with the highest bed count: ${HospitalDataAnalysis.stateWithHighestBedCount(dataset)}\n" +
+    f"Average Covid to Bed Ratio overall: ${HospitalDataAnalysis.covidBedRatio(dataset)}%.2f\n" +
     "===========================================================================================\n" +
-    s"Average Admissions for Each State\n" +
-    "------------------------------------------------------------------------------------------\n")
+    "Average Admissions for Each State\n" +
+    "===========================================================================================\n" +
+    f"${"STATE"}%20s \t| AVERAGE COVID ADMISSIONS \t| AVERAGE PUI ADMISSIONS\n"+
+    "===========================================================================================")
 
   //Averages block
-  HospitalDataAnalysis.getAverageAdmissionsByCategory(dataset).foreach{
+  HospitalDataAnalysis.averageAdmissionsByCategory(dataset).foreach{
     case (state, values) =>
-      println(s"$state \nAverage Covid: ${values.head} \nAverage Pui: ${values(1)}  \nAverage Total: ${values(2)}\n"
-      + "------------------------------------------------------------------------------------------")
+      println(f"$state%20s \t| ${values.head}%24.2f \t| ${values(1)}%22.2f\n"
+      + "___________________________________________________________________________________________")
   }
 
-//Effective running
-//  hospitalDataset.foreach(line => println(line))
+/* Problems causing poor marks:
+- Naming convention
+- records.Tail
+- A lot of get methods.
+- Possibly might need a new data structure.
+ */
+
+/*
+Parametric Poly - with maps, change Trait based to Map based transformation
+Naming Convention change
+records.tail
+ */
+
